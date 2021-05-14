2804f16b9ce3b9fb1b35e115fb1f47a61bf95d46
ishika22 <ishuagarwal2210@gmail.com>
2021-05-15 00:11:34 +0530

    login component changes into master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+// import { TestComponent } from './test/test.component';
 // import { IconsModule } from 'angular-bootstrap-md';
 // import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,11 +14,17 @@ import { MatButtonModule } from '@angular/material/button';
 
 // search module
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
+    // TestComponent,
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent,
     
   ],
   imports: [
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class TestComponent implements OnInit {
+export class HeaderComponent implements OnInit {
   userName='Sachin Sharma';
   title = 'Angular Search Using ng2-search-filter';
-  
   constructor() { }
 
   ngOnInit(): void {
